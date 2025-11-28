9c54b47724b2564f555f05c8fe1f1e856f2a7e7b
Lukas Seeger <lseeger@.42heilbronn.de>
2025-11-28 14:28:01 +0100

    Pmerge me setup

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+class PmergeMe {
+private:
+
+public:
+    // Canonical Form
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
+
+    // Special Constructor
+
+    // Functions
+};
